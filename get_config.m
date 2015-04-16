762d08f94f5a16377b1b9cc8fdfe5c0cd31d4cb3
Jacob Dink <jwdink1@gmail.com>
2015-04-15 23:45:34 -0500

    defaults in get_config

--- get_config.m
+++ get_config.m
@@ -1,7 +1,11 @@
 %% FXN_get_config
-function [value] = get_config (name)
+function [value] = get_config (name, default)
 
 global session
+
+if nargin < 2
+    default = [];
+end
 
 matching_param = find(cellfun(@(x) strcmpi(x, name), {session.config.Parameter}));
 value = [session.config(matching_param).Setting]; %#ok<FNDSB>
@@ -10,4 +14,9 @@ value = [session.config(matching_param).Setting]; %#ok<FNDSB>
 if ischar(value) || iscell(value)
     value = strrep(value, '"', '');
 end
+
+if isempty(value)
+    value = default;
+end
+
 end
